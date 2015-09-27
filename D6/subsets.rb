cee20e5595f57832ce637b0c6274be504e7accb8
Varun Patel <patelv1991@gmail.com>
2015-09-27 15:38:38 -0700

    add subsets

---
+++ D6/subsets.rb
@@ -0,0 +1,11 @@
+# Write a function that takes an array and returns all of its subsets.
+# How many sets will it return?
+
+def subsets(arr)
+  return [[]] if arr.length == 0
+
+  val = arr[0]
+  subs = subsets(arr.drop(1))
+  new_subs = subs.map { |sub| [val] + sub }
+  subs + new_subs
+end
