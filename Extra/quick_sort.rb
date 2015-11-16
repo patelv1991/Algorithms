4f71f917f553f8f2fd832c3a561e12d55a365ac0
Varun Patel <patelv1991@gmail.com>
2015-11-15 19:57:36 -0800

    add a naive way to solve quick sort

--- Extra/quick_sort.rb
+++ Extra/quick_sort.rb
@@ -2,5 +2,20 @@
 
 # naive version
 def quick_sort(arr)
+  return arr if arr.length < 2
+  arr.shuffle!
+  pivot = arr.shift
 
+  left = []
+  right = []
+
+  arr.each do |num|
+    if num <= pivot
+      left << num
+    else
+      right << num
+    end
+  end
+
+  quick_sort(left) + [pivot] + quick_sort(right)
 end
