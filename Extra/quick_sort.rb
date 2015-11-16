58fbe1911633ae831d227e460bf9056a9f02ea9a
Varun Patel <patelv1991@gmail.com>
2015-11-15 21:06:34 -0800

    add space efficient version of quick_sort

--- Extra/quick_sort.rb
+++ Extra/quick_sort.rb
@@ -1,6 +1,6 @@
 # Implement quick sort
 
-# naive version
+# Naive version
 def quick_sort(arr)
   return arr if arr.length < 2
   arr.shuffle!
@@ -18,4 +18,34 @@ def quick_sort(arr)
   end
 
   quick_sort(left) + [pivot] + quick_sort(right)
+end
+
+# More advanced solution
+class Array
+  def quick_sort!(start = 0, len = length)
+    return self if len < 2
+    swap!(start, rand(len) + start)
+    pivot = partition!(start, len)
+    quick_sort!(start, pivot - start)
+    quick_sort!(pivot + 1, len - pivot + start - 1)
+  end
+
+  def partition!(start, len)
+    pivot = start
+    i = pivot.next
+    while i < start + len
+      if self[i] < self[pivot]
+        swap!(i, pivot.next)
+        swap!(pivot, pivot.next)
+        pivot += 1
+      end
+      i += 1
+    end
+    pivot
+  end
+
+  def swap!(idx1, idx2)
+    self[idx1], self[idx2] = self[idx2], self[idx1]
+  end
+
 end
